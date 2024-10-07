11136faf6d0d44ed07e5d81bddffe8daf1fca9d4
NafiaSyed <nafiasyed25@gmail.com>
2024-10-07 20:30:57 +0530

    fn-exp file added

---
+++ function-expression.js
@@ -0,0 +1,8 @@
+const calculateArea = function(length, width) {
+    return length * width;
+};
+
+let length = 10;
+let width = 5;
+const area = calculateArea(length, width);
+console.log(`The area of the rectangle is ${area} square units`); // Outputs: The area of the rectangle is 50 square units
